c1d6e52bf85f5b0f37777a3976513c1958abbff6
SanJiang <changsanjiang@gmail.com>
2017-12-30 18:07:32 +0800

    Fixed `隐藏了导航栏的根控制器`pop时, 视图层级的问题.

--- SJVideoPlayerBackGR/UINavigationController+SJVideoPlayerAdd.m
+++ SJVideoPlayerBackGR/UINavigationController+SJVideoPlayerAdd.m
@@ -383,30 +383,36 @@ static __weak UIViewController *_tmpShowViewController;
 
 - (void)SJ_ViewWillBeginDragging {
     [self.view endEditing:YES]; // 让键盘消失
-    NSInteger index = _window.subviews.count - 2;
-    if ( index < 0 ) index = 0;
-    [_window insertSubview:self.SJ_screenshotView atIndex:index];
+    
+    UIWindow *window = self.view.window;
+    [window.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ( ![self.view isDescendantOfView:obj] ) return ;
+        *stop = YES;
+        // Move the `screenshot` to the bottom of the `current view`.
+        [window insertSubview:self.SJ_screenshotView atIndex:idx - 1];
+    }];
+
     self.SJ_screenshotView.hidden = NO;
     
     [self SJ_findingPossibleRootScrollView].scrollEnabled = NO;
     
-    // call block
-    if ( self.topViewController.sj_viewWillBeginDragging ) self.topViewController.sj_viewWillBeginDragging(self.topViewController);
-    
     // begin animation
     self.SJ_screenshotView.transform = CGAffineTransformMakeTranslation(SJ_Shift, 0);
+    
+    // call block
+    if ( self.topViewController.sj_viewWillBeginDragging ) self.topViewController.sj_viewWillBeginDragging(self.topViewController);
 }
 
 - (void)SJ_ViewDidDrag:(CGFloat)offset {
     self.view.transform = CGAffineTransformMakeTranslation(offset, 0);
     
-    // call block
-    if ( self.topViewController.sj_viewDidDrag ) self.topViewController.sj_viewDidDrag(self.topViewController);
-    
     // continuous animation
     CGFloat rate = offset / self.view.frame.size.width;
     self.SJ_screenshotView.transform = CGAffineTransformMakeTranslation(SJ_Shift - SJ_Shift * rate, 0);
     [self.SJ_screenshotView setShadeAlpha:1 - rate];
+    
+    // call block
+    if ( self.topViewController.sj_viewDidDrag ) self.topViewController.sj_viewDidDrag(self.topViewController);
 }
 
 - (void)SJ_ViewDidEndDragging:(CGFloat)offset {
@@ -506,3 +512,5 @@ static __weak UIViewController *_tmpShowViewController;
 }
 
 @end
+
+
